2c91998625217f88334a7ba95cfcbf4690b210a0
Divyaraj Bakrola <divyaraj@sfs-wifi-dhcp-10-142-6-217.wifi.sfsu.edu>
2018-12-11 23:25:50 -0800

    added places view controller and initialized places data

---
+++ Vizit/PlacesViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlacesViewController.h
+//  Vizit
+//
+//  Created by Divyaraj Bakrola on 12/11/18.
+//  Copyright © 2018 Vizit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlacesViewController : UIViewController
+
+@end
---
+++ Vizit/PlacesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PlacesViewController.swift
+//  Vizit
+//
+//  Created by Divyaraj Bakrola on 12/11/18.
+//  Copyright © 2018 Vizit. All rights reserved.
+//
+
+import UIKit
+
+class PlacesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
