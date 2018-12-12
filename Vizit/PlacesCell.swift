250bc1784f30a85931278b3655945d3aa9662162
Divyaraj Bakrola <divyaraj@sfs-wifi-dhcp-10-142-6-217.wifi.sfsu.edu>
2018-12-12 00:26:44 -0800

    added places cell and places data

---
+++ Vizit/PlacesCell.swift
@@ -0,0 +1,26 @@
+//
+//  PlacesCell.swift
+//  Vizit
+//
+//  Created by Divyaraj Bakrola on 12/12/18.
+//  Copyright © 2018 Vizit. All rights reserved.
+//
+
+import UIKit
+
+class PlacesCell: UITableViewCell {
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
